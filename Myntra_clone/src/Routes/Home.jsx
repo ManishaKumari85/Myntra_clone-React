eb846385a5c84bbbcbf93ab04a5c1321e9f6f861
unknown <manishabharti236@gmail.com>
2024-02-20 14:44:17 +0530

    continue

---
+++ Myntra_clone/src/Routes/Home.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+import Homeitem from "../Components/Homeitem"
+
+const Home = () => {
+  const item = {
+    id: "008",
+    image: "images/8.jpg",
+    company: "Nivea",
+    item_name: "Men Fresh Deodrant 150ml",
+    original_price: 285,
+    current_price: 142,
+    discount_percentage: 50,
+    return_period: 14,
+    delivery_date: "10 Oct 2023",
+    rating: {
+      stars: 4.2,
+      count: 5200,
+    },
+  };
+
+  return (
+    <>
+      <main>
+        <div className="items-container">
+          <Homeitem item={item} />
+        </div>
+      </main>
+    </>
+  );
+};
+
+export default Home;
